4f0249d0621f43ceb2b306f38d191c9f55cece4c
Zilvinas Juodis <zilvinoj@gmail.com>
2022-05-29 14:36:06 +0300

    ok

---
+++ Homework/Clock/Calculator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clock
+{
+    class Calculator
+    {
+        private const int hoursDegreeStep = 30;
+        private const int minutesDegreeStep = 6;
+        private const double HVsMinStep = 0.5;
+
+        public int Hours { get; set; }
+        public int Minutes { get; set; }
+
+        public Calculator(int hours, int minutes)
+        {
+            if (hours > 12 || hours < 1)
+            {
+                throw new Exception("Entered hours are not in 1-12 range, please eneter correct hours");
+            }
+            else
+            {
+                this.Hours = hours;
+            }
+            if (minutes > 60 || minutes < 0)
+            {
+                throw new Exception("Entered minutes are not in 0-60 range, please enter correct minutes");
+            }
+            else
+            {
+                this.Minutes = minutes;
+            }
+        }
+
+        public (double, int) GetDegrees()
+        {
+            double hoursDegrees = hoursDegreeStep * Hours + HVsMinStep * Minutes;
+            int minutesDegrees = minutesDegreeStep * Minutes;
+            return (hoursDegrees, minutesDegrees);
+        }
+
+        public double GetArrowsAngle()
+        {
+            double degrees = Math.Abs(360 - GetDegrees().Item1 + GetDegrees().Item2 - 360);
+            if (degrees > 180)
+            {
+                degrees = 360 - GetDegrees().Item1 + GetDegrees().Item2;
+            }
+            return degrees;
+        }
+    }
+}
---
+++ Homework/Clock/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Clock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("### Clock angle app ###");
+                Console.WriteLine();
+                Console.WriteLine("Enter hours: ");
+                int hours = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter minutes: ");
+                int minutes = int.Parse(Console.ReadLine());
+
+                Calculator calculator = new Calculator(hours, minutes);
+
+                Console.WriteLine();
+                Console.WriteLine($"Arrows angle is: {calculator.GetArrowsAngle()}°");
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine(error.Message);
+            }
+        }
+    }
+}
